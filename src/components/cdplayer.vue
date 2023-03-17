321fe0e367c23aa4d1fe1a817be5720d5847e3e1
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-03-17 16:09:19 +0800

    03/17

--- src/components/cdplayer.vue
+++ src/components/cdplayer.vue
@@ -1,17 +1,20 @@
 <template>
-  <div>
-    <h1>{{ currentSong.title }}</h1>
-    <h1>{{ currentSong.artist }}</h1>
-    <button @click="togglePlay">{{ isPlaying ? "Pauce" : "Play" }}</button>
-    <button class="p-5" @click="stop">Stop</button>
-    <button @click="skip">Skip</button>
+  <div class="music-player">
+    <h2>{{ currentSong.title }}</h2>
+    <h4>{{ currentSong.artist }}</h4>
+    <img :src="currentSong.albumCover" alt="" />
+    <div class="controls">
+      <button @click="playOrPause">{{ isPlaying ? "暂停" : "播放" }}</button>
+      <button @click="stop">停止</button>
+      <button @click="skip">下一曲</button>
+    </div>
   </div>
 </template>
 <script setup>
 import { ref, watch } from "vue";
 const songList = ref([
-  { title: "Song 1", artist: "JSON01", url: "song1.mp3" },
-  { title: "Song 2", artist: "JSON02", url: "song2.mp3" },
+  { title: "Song 1", artist: "JSON01", url: "../assets/suchmos.mp3" },
+  { title: "Song 2", artist: "JSON02", url: "@/assets/suchmos.mp3" },
   { title: "Song 3", artist: "JSON03", url: "song3.mp3" },
 ]);
 const currentSong = ref(songList.value[0]);
@@ -55,10 +58,10 @@ const loadSong = () => {
 loadSong();
 
 const play = () => {
-  audio.play;
+  audio.play();
 };
 const pause = () => {
-  audio.pause;
+  audio.pause();
 };
 
 loadSong();
