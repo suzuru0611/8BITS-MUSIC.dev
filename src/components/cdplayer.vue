b5ab2cbc5e53f7bc2b77b238a316d5e285ec6e0e
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-03-17 15:46:02 +0800

    03/17

--- src/components/cdplayer.vue
+++ src/components/cdplayer.vue
@@ -1,31 +1,67 @@
 <template>
   <div>
-    <select v-model="currentSong">
-      <option v-for="song in songlist" :value="song">{{ song }}</option>
-    </select>
-    <button @click="playerPause">{{ isPlaying ? "Pauce" : "Play" }}</button>
-    <button @click="stop">Stop</button>
+    <h1>{{ currentSong.title }}</h1>
+    <h1>{{ currentSong.artist }}</h1>
+    <button @click="togglePlay">{{ isPlaying ? "Pauce" : "Play" }}</button>
+    <button class="p-5" @click="stop">Stop</button>
     <button @click="skip">Skip</button>
   </div>
 </template>
 <script setup>
-import { ref } from "vue";
-const songList = ref(["Song A", "Song B", "Song C"]);
+import { ref, watch } from "vue";
+const songList = ref([
+  { title: "Song 1", artist: "JSON01", url: "song1.mp3" },
+  { title: "Song 2", artist: "JSON02", url: "song2.mp3" },
+  { title: "Song 3", artist: "JSON03", url: "song3.mp3" },
+]);
 const currentSong = ref(songList.value[0]);
 const isPlaying = ref(false);
 
-const playerPause = () => {
+const audio = new Audio();
+const togglePlay = () => {
+  if (isPlaying.value) {
+    audio.pause();
+  } else {
+    audio.play();
+  }
   isPlaying.value = !isPlaying.value;
 };
+
+const stop = () => {
+  audio.pause();
+  audio.currentTime = 0;
+  isPlaying.value = false;
+};
+
 const skip = () => {
   const currentIndex = songList.value.indexOf(currentSong.value);
   if (currentIndex === songList.value.length - 1) {
-    currentSong.value = songlist.value[0];
+    currentSong.value = songList.value[0];
   } else {
     currentSong.value = songList.value[currentIndex + 1];
   }
   isPlaying.value = true;
+  loadSong();
 };
+
+const loadSong = () => {
+  audio.src = currentSong.value.url;
+  audio.load();
+  if (isPlaying.value) {
+    audio.play();
+  }
+};
+
+loadSong();
+
+const play = () => {
+  audio.play;
+};
+const pause = () => {
+  audio.pause;
+};
+
+loadSong();
 </script>
 
 <style scoped></style>
