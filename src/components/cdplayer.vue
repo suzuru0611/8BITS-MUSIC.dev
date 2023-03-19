b984f21788af156b10c6b615f22dda3b261ef46e
suzuru0611 <suzuru0611@gmail.com>
2023-03-19 21:20:32 +0800

    03/19

--- src/components/cdplayer.vue
+++ src/components/cdplayer.vue
@@ -1,12 +1,47 @@
 <template>
-  <div class="music-player">
-    <h2>{{ currentSong.title }}</h2>
-    <h4>{{ currentSong.artist }}</h4>
-    <img :src="currentSong.albumCover" :alt="currentSong.alt" />
-    <div class="controls">
+  <div class="flex h-screen w-screen flex-col justify-center bg-yellow-500">
+    <div class="flex flex-col items-center">
+      <p class="font-sans text-4xl font-bold">{{ currentSong.title }}</p>
+      <p class="pt-3 text-2xl">{{ currentSong.artist }}</p>
+    </div>
+    <div class="flex w-full justify-center pt-10 pb-10">
+      <div class="w-4/6 lg:w-auto">
+        <img :src="currentSong.albumCover" :alt="currentSong.alt" />
+      </div>
+    </div>
+    <div class="flex w-full flex-row justify-center">
       <button @click="togglePlay">{{ isPlaying ? "暂停" : "播放" }}</button>
-      <button @click="stop">停止</button>
-      <button @click="skip">下一曲</button>
+      <button @click="next">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#d0021b"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polygon points="5 3 19 12 5 21 5 3"></polygon>
+        </svg>
+      </button>
+      <button @click="next">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="#d0021b"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        >
+          <polygon points="13 19 22 12 13 5 13 19"></polygon>
+          <polygon points="2 19 11 12 2 5 2 19"></polygon>
+        </svg>
+      </button>
     </div>
   </div>
 </template>
@@ -96,7 +131,7 @@ const stop = () => {
   isPlaying.value = false;
 };
 
-const skip = () => {
+const next = () => {
   const currentIndex = songList.value.indexOf(currentSong.value);
   if (currentIndex === songList.value.length - 1) {
     currentSong.value = songList.value[0];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('/hero.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
