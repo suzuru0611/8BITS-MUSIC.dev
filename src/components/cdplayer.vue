d839b07c785db9f412a5224fe574e73102672c4b
suzuru0611 <120362718+suzuru0611@users.noreply.github.com>
2023-03-22 18:55:44 +0800

    03/22

--- src/components/cdplayer.vue
+++ src/components/cdplayer.vue
@@ -2,23 +2,6 @@
   <div
     class="flex h-screen w-screen flex-col justify-center overflow-hidden bg-yellow-500"
   >
-    <!-- <section class="nes-container">
-      <section class="message-list">
-        <section class="message -left">
-          <i class="nes-bcrikko"></i>
-          <div class="nes-balloon from-left">
-            <p>Hello NES.css</p>
-          </div>
-        </section>
-
-        <section class="message -right">
-          <div class="nes-balloon from-right">
-            <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
-          </div>
-          <i class="nes-bcrikko"></i>
-        </section>
-      </section>
-    </section> -->
     <div class="flex flex-col items-center">
       <section class="nes-container with-title w-4/6 lg:w-2/6">
         <h2 class="title">{{ currentSong.artist }}</h2>
